f6d0fa1c4a34e89b3fae6a6f2b4d91d84ca177e0
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2022-08-09 20:58:36 +1000

    Initialise template

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import '@dpc-sdp/ripple-ui-core/style'
+import '@dpc-sdp/ripple-ui-core/style/components'
+import App from './App.vue'
+
+createApp(App).mount('#app')
