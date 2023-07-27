4cba61649bb3265f89b276530b81d0a961235ebc
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2023-07-27 14:58:56 +1000

    remove demo module

--- server/plugins/demo.ts
+++
@@ -1,8 +0,0 @@
-import { defineNitroPlugin } from 'nitropack/dist/runtime/plugin'
-import type { NitroApp } from 'nitropack'
-import mapping from './../../mapping'
-
-export default defineNitroPlugin((nitroApp: NitroApp) => {
-  /* Replace this with your own content type or delete as required */
-  nitroApp.tide?.pageApi.setContentType('demo', mapping)
-})
