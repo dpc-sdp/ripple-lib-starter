a401e8314b3088d69db8858dc4e653cc95d4e275
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2023-10-09 15:19:55 +1100

    Add examples branch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,4 @@
 // See: https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({})
+export default defineNuxtConfig({
+  extends: ['examples/content-type', 'examples/custom-page']
+})
