1662bca607a3b4a735a939f52581272fec906b40
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2025-06-05 15:31:48 +1000

    chore: project created with projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,8 @@
 import { SdpRplLayer } from '@dpc-sdp/ripple-sdp-projen-config'
-
 const project = new SdpRplLayer({
-  name: 'ripple-lib-starter',
-  defaultReleaseBranch: 'main'
+   name: 'ripple-lib-starter',
+   defaultReleaseBranch: 'main'
+   addCypress: true,
+  addExamples: true,
 })
-
 project.synth();
